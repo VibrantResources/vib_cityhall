0371698d2c41117ab5ce2f65a6d34479711c58f5
Vibrant <127861623+VibrantRP@users.noreply.github.com>
2025-01-26 23:48:16 +0000

    Update: Add "How-to" text to config

--- config.lua
+++ config.lua
@@ -16,6 +16,16 @@ Config.Jobs = {
         lumberjack = {
             job_Label = "Lumberjack",
             job_Description = "Maintain the trees of Paleto Forest with steel",
+            how_To_Guide = {[[
+                # Overview
+                From the labs of Vibrant Enterprise in Los Santos emerges the fabricator, crafted from military-grade titanium alloys and advanced quantum processing units. Designed for efficiency and precision, this sleek device transforms base materials—steel, electronics, plastics, and precision components—into custom items. It integrates seamlessly into operational environments, embodying Vibrant Enterprise's commitment to innovation and excellence in manufacturing within the competitive underworld of Los Santos.
+
+                # A few things:
+                1. You need to charge the battery, there is documentation on this in the main documentation.
+                2. To load it, you need to click 'use'. It will open the fabricator and you place your items.
+                3. To use it, you need to click 'Activate'. This will attempt to make something with the items you have placed inside.
+                4. If nothing happens, then you did not get the right combination.
+            ]]},
             location = vector4(-571.53, 5328.45, 70.21, 244.0),
         },
     },
--- menus/jobs.lua
+++ menus/jobs.lua
@@ -38,6 +38,15 @@ RegisterNetEvent('cityhall:client:JobsListMenu', function(jobsList)
         headerMenu[#headerMenu + 1] = {
             title = v.job_Label,
             description = v.job_Description,
+            onSelect = function()
+                local alert = lib.alertDialog({
+                    header = 'How to guide',
+                    content = "https://cdn.discordapp.com/attachments/1324514878869733460/1331369778761302027/image.png?ex=6794013a&is=6792afba&hm=73a12b1937d7b34a67481632a34b686438c821d13a172eaa80aa85044e959dd8&",
+                    centered = true,
+                    cancel = false,
+                    size = "xl",
+                })
+            end,
             icon = 'fa-solid fa-question',
             iconColor = "yellow",
         }
